6bbd6e0e5ae75d8d896b544b9493cabd8c13c0b4
DavidSillo273 <20240273@ricaldone.edu.sv>
2026-04-22 16:20:08 -0600

    fix: corregir error crítico de APP.jsx en las rutas

--- FrontEnd/DayReadyWeb/DayReadyWeb/src/App.jsx
+++ FrontEnd/DayReadyWeb/DayReadyWeb/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginAdmin from './Pages/privateWeb/loginAdmin';
-import RegisterUser from './Pages/privateWebregisterUser';
-import RecoveryPass from './Pages/privateWebrecoveryPass';
+import RegisterUser from './Pages/privateWeb/registerUser';
+import RecoveryPass from './Pages/privateWeb/recoveryPass';
 
 function App() {
   return (
